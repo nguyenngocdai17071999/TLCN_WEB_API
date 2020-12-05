56cb86034ff4d9b43c347a3fc361d5e4f4981a5f
nguyenngocdai17071999 <73878116+nguyenngocdai17071999@users.noreply.github.com>
2020-12-05 13:00:40 +0700

    dai

--- TLCN_WEB_API/TLCN_WEB_API/Models/Dish.cs
+++ TLCN_WEB_API/TLCN_WEB_API/Models/Dish.cs
@@ -9,7 +9,7 @@ namespace TLCN_WEB_API.Models
     {
         public int Dish_ID { get; set; }
         public string DishName { get; set; }
-        public int DishPrice { get; set; }
+        public string DishPrice { get; set; }
         public string DishPicture { get; set; }
         public int DishType_ID { get; set; }
         public int Menu_ID { get; set; }
