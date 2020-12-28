1fede85dbbb267b1804f194e7cdb13951ab6ec3e
nguyenngocdai17071999 <73878116+nguyenngocdai17071999@users.noreply.github.com>
2020-12-28 16:25:45 +0700

    dai

--- TLCN_WEB_API/TLCN_WEB_API/Models/UserCommentInfo.cs
+++ TLCN_WEB_API/TLCN_WEB_API/Models/UserCommentInfo.cs
@@ -7,6 +7,15 @@ namespace TLCN_WEB_API.Models
 {
     public class UserCommentInfo
     {
+        private string userID;
+
+        public UserCommentInfo(string userName, string picture, string userID)
+        {
+            UserName = userName;
+            Picture = picture;
+            this.userID = userID;
+        }
+
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Picture { get; set; }
